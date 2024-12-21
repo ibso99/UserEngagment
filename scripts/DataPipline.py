336a2b3231778a2a0df6cd00ca3e30975dd9cf85
ibso99 <123656921+ibso99@users.noreply.github.com>
2024-12-21 10:03:38 +0300

    Load data from sql and conducted EDA

--- scripts/DataPipline.py
+++ scripts/DataPipline.py
@@ -1,15 +1,59 @@
 import pandas as pd
 import numpy as np
 import datetime
+from sqlalchemy import create_engine
+
+import os
+import psycopg2 
+import pandas as pd
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+# load environmemt variable from .dotenv
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 class DataPipeline:
         ''' Data Engineering Pipe line for loading cleaning and performing 
         aggregate values'''
         #################################################
 
-        def Data_Loader(data_path):
+        def load_data_using_sqlalchemy(query):
+            try:
+                connection_string = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-            return pd.read_csv(data_path)
+                engine = create_engine(connection_string)
+
+                df = pd.read_sql_query(query, engine)
+
+                return df 
+            except Exception as e:
+                print(f'An Error Occured:{e}')
+                return None
+            
+        def load_data_from_postgres(query):
+            try:
+                connection = psycopg2.connect(
+                    host = DB_HOST,
+                    port = DB_PORT,
+                    database=DB_NAME,
+                    user = DB_USER,
+                    password = DB_PASSWORD
+                )
+
+                df = pd.read_sql_query(query, connection)
+
+                connection.close()
+                return df
+            
+            except Exception as e:
+                print(f"an error occured: {e}")
+            return None
         
         ################################################
 
@@ -80,6 +124,46 @@ class DataPipeline:
                                     'Total_Upload']
 
             return aggregated_df
+       
+
+        def excute_telecom_queries(db_url):
+            engine = create_engine(db_url)
+
+            # 1 Count of unique IMSIs
+            unique_imsi_count = pd.read_sql_query("""
+        SELECT COUNT(DISTINCT "IMSI") AS unique_imsi_count 
+                                                FROM xdr_data;
+
+        """, engine)
+            # 2 Average Duration of Calls
+            average_duration = pd.read_sql_query("""
+        SELECT AVG("Dur. (ms)") AS average_duration
+                                                FROM xdr_data
+                                                WHERE "Dur. (ms)" IS NOT NULL; """, engine)
+            # 3. Total Data Usage per User
+            total_data_usage = pd.read_sql_query("""
+        SELECT "IMSI",
+                                                SUM("Total UL (Bytes)") AS total_ul_bytes,
+                                                SUM("Total DL (Bytes)") AS total_dl_bytes
+                                                FROM xdr_data
+                                                GROUP BY "IMSI"
+                                                ORDER BY total_dl_bytes DESC
+                                                LIMIT 10; """, engine)
+            # Average RTT by Last Location Name 
+            avg_rtt_by_location = pd.read_sql_query("""
+        SELECT "Last Location Name",
+                                                    AVG("Avg RTT DL (ms)") AS avg_rtt_dl,
+                                                    AVG("Avg RTT UL (ms)") AS avg_rtt_ul
+                                                    FROM xdr_data
+                                                    GROUP BY "Last Location Name" 
+                                                    HAVING COUNT(*) > 10 
+                                                    ORDER BY avg_rtt_dl DESC;""", engine)
+            return {
+                'unique_imsi_count': unique_imsi_count,
+                'average_duration' : average_duration,
+                'total_data_usage' : total_data_usage,
+                'avg_rtt_by_location': avg_rtt_by_location,
+            }
         
 
         
